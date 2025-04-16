34a9ad40486e8240b3eddc168bc8e64a4737c36d
Yogya Arora <93470486+YogyaAlpha@users.noreply.github.com>
2025-04-16 16:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -491,6 +491,14 @@ function processVerification(reportNumber) {
     document.getElementById("Shapey").textContent = "Cushion Modified";
     modaly.style.display = "flex";
   }
+
+      else if (reportNumber == 23786956) {
+    document.getElementById("reportNoy").textContent = reportNumber;
+    document.getElementById("measurementsy").textContent = "N/A";
+    document.getElementById("weighty").textContent = "7.40 ratti";
+    document.getElementById("Shapey").textContent = "Oval Modified";
+    modaly.style.display = "flex";
+  }
   
   else if (reportNumber == 1221) {
     document.getElementById("reportNoy").textContent = reportNumber;
